74373e82820b88e30555a37b3ac27bad1c0d7198
Diego Lapiduz <diego.lapiduz@gsa.gov>
2014-12-18 18:02:25 -0800

    Use newer LWRP for postgres setup

--- attributes/db.rb
+++ attributes/db.rb
@@ -3,30 +3,4 @@ default.midas.database.password = 'midas'
 default.midas.database.hostname = 'localhost'
 default.midas.database.name = 'midas'
 
-default.postgresql.database.name = 'midas'
-default.postgresql.database.encoding = 'unicode'
-default.postgresql.databases = [
-  {
-    name: node.midas.database.name,
-    owner: node.midas.database.username
-  }
-]
-default.postgresql.users = [
-  {
-    username: node.midas.database.username,
-    password: node.midas.database.password,
-    superuser: false,
-    createdb: true,
-    login: true
-  }
-]
-default.postgresql.pg_hba = [
-  {
-    type: 'hostssl',
-    db: 'all',
-    user: 'all',
-    addr: '0.0.0.0/0',
-    method: 'md5'
-  }
-]
-default.postgresql.password = ''
+default['postgresql']['password']['postgres'] = 'postgres'
--- recipes/db.rb
+++ recipes/db.rb
@@ -1,3 +1,30 @@
 include_recipe 'postgresql'
+include_recipe 'postgresql::client'
 include_recipe 'postgresql::server'
-include_recipe 'postgresql::libpq'
+include_recipe 'database::postgresql'
+
+# Set up databases and users
+db_connection_info = {
+  host: node['midas']['database']['hostname'],
+  username: 'postgres',
+  password: node['postgresql']['password']['postgres'],
+  port: 5432
+}
+
+postgresql_database node['midas']['database']['name'] do
+  connection db_connection_info
+  action :create
+end
+
+postgresql_database_user node['midas']['database']['username'] do
+  connection db_connection_info
+  password node['midas']['database']['password']
+  action :create
+end
+
+postgresql_database_user node['midas']['database']['username'] do
+  connection db_connection_info
+  database_name node['midas']['database']['name']
+  privileges [:all]
+  action :grant
+end
