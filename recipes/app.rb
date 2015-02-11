2ee0ea1abc80e5e3c356da6f10c5a14c08ab5c21
Dave Cole <david.cole@gsa.gov>
2015-02-11 12:36:15 -0500

    Set NODE_ENV as environment var instead of config

--- recipes/app.rb
+++ recipes/app.rb
@@ -26,7 +26,6 @@ template  "#{node.midas.deploy_dir}/config/local.js" do
   variables(
     app_id: node.midas.app_id,
     app_host: node.midas.app_host,
-    app_environment: node.midas.environment,
     db_host: node.midas.database.hostname,
     db_user: node.midas.database.username,
     db_password: node.midas.database.password,
@@ -139,6 +138,7 @@ template "/etc/init/midas.conf" do
   variables(
     working_dir: node.midas.deploy_dir,
     app_user: node.midas.user,
+    app_environment: node.midas.environment,
     newrelic_appname: node.midas.newrelic.appname,
     newrelic_licensekey: node.midas.newrelic.licensekey
   )
